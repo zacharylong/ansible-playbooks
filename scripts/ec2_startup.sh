b50fdf07d54d280d87b924a1fca804f8315a1ea3
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-06-29 11:24:28 -0400

    Never associated the 2nd public subnet with the route tables, internet gateways, nats, etc. Added script data to associate.

--- scripts/ec2_startup.sh
+++ scripts/ec2_startup.sh
@@ -3,4 +3,6 @@
 export IMAGE_GALLERY_BOOTSTRAP_VERSION="1.0"
 
 aws s3 cp s3://edu.au.cc.m5-image-gallery-superunique-config/ec2-prod-latest.sh ./
+aws s3 cp s3://edu.au.cc.m5-image-gallery-superunique-config/ec2-prod-1.1.sh ./
 /usr/bin/bash ec2-prod-latest.sh
+/usr/bin/bash ec2-prod-1.1.sh
