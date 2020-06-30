de256acc99d6a61136d41b173bcc95af0f8e8c62
Zachary Long <42480238+zacharylong@users.noreply.github.com>
2020-06-29 22:06:02 -0400

    Creates an EC2 instance that copies the S3 buckt initial deploy data from a development machine we did in Module 4

---
+++ scripts/deploy_webapp.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# Install packages
+yum -y update
+yum install -y emacs-nox nano tree python3
+yum install -y git
+
+# Configure/install custom software
+cd /home/ec2-user
+git clone https://github.com/zacharylong/python-image-gallery.git
+chown -R ec2-user:ec2-user python-image-gallery
+su ec2-user -c "cd ~/python-image-gallery && pip3 install -r requirements.txt --user"
+
+# Start/enable services
+systemctl stop postfix
+systemctl disable postfix
+
+# copy config files to s3 bucket
+
+BUCKET="edu.au.cc.m5-image-gallery-superunique-config"
+
+cd /home/ec2-user/python-image-gallery
+
+aws s3 cp ec2-scripts/ec2-prod-1.1.sh s3://${BUCKET}
+aws s3 cp ec2-scripts/ec2-prod-latest.sh s3://${BUCKET}
+aws s3 sync nginx s3://${BUCKET}/nginx
